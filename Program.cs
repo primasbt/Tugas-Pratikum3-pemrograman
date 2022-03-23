c711bfaf31e25c8caf00a56f9ded6fe6479c80e0
Primasbt <91203079+primasbt@users.noreply.github.com>
2022-03-23 16:30:17 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace taxi
+{
+    class Car
+    {
+        static void Main(string[] args)
+        {
+            Taxi taxi = new Taxi();
+
+            taxi.Driver = "Joe";
+            taxi.OnDuty = true;
+            taxi.JumlahPelanggan = 10;
+
+            taxi.TaxiInfo();
+            taxi.JemputPenumpang();
+            taxi.DropOff();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Taxi.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace taxi
+{
+    class Taxi
+    {
+
+        public string Driver { get; set; }
+        public bool OnDuty { get; set; }
+        public int JumlahPelanggan { get; set; }
+
+        public void TaxiInfo()
+        {
+            string duty;
+
+            if (OnDuty)
+            {
+                duty = "ONLINE";
+            }
+            else
+            {
+                duty = "OFFLINE";
+            }
+
+            Console.WriteLine("Nama Driver: {0}", Driver);
+            Console.WriteLine("Status: {0}", duty);
+            Console.WriteLine("Jumlah Pelanggan: {0}", JumlahPelanggan);
+        }
+
+        public void JemputPenumpang()
+        {
+            Console.WriteLine("{0} sedang menjemput penumpang", Driver);
+        }
+
+        public void DropOff()
+        {
+            Console.WriteLine("{0} Telah selesai mengantar penumpang", Driver);
+        }
+
+    }
+}
+
+    
+    
+
+   
+
